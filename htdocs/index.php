c15b8ecdaf774f3e60c9c28730750a7c0b69c6b7
misaki.y <misaki.yokota@epkotsoftware.co.jp>
2024-05-14 19:03:25 +0900

    ブラウザ確認済

---
+++ htdocs/index.php
@@ -0,0 +1,35 @@
+<?php
+const DB_HOST = 'php8-2-db-host'; 
+const DB_PORT = 3306; 
+const DB_DATABASE = 'php_db'; 
+const DB_USERNAME = 'root';
+const DB_PASSWORD = 'root'; 
+const DB_CHARSET = 'utf8mb4';
+
+$dsn = 'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_DATABASE . ';charset=' . DB_CHARSET;
+try {
+    $dbh = new PDO($dsn, DB_USERNAME, DB_PASSWORD);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test</title>
+    <link rel="stylesheet" href="css/style.css" >
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
+    <script>
+    //自作jsを作成
+    </script>
+</head>
+<body>
+    これはテストです
+</body>
+</html>
+
